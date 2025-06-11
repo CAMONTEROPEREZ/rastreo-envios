3dd8fa8598f1d4eda3816a05e3d736ca45c20ebc
CAMONTEROPEREZ <78057408+CAMONTEROPEREZ@users.noreply.github.com>
2025-06-11 16:09:14 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,12 @@ interface Pedido {
     lng: number;
   };
   ultimaActualizacion: string;
+  ruta?: {
+    lat: number;
+    lng: number;
+  }[]; // <- NUEVO: ruta como propiedad opcional
 }
+
 
 const pedidosEjemplo: Pedido[] = [
  // nuevo campo `ruta` con paradas intermedias
